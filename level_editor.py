b308666fbe0267c0b282557a1c82c8b8b0ad3177
浅川 咲人 <k024g1017@g.neec.ac.jp>
2026-05-18 05:26:21 +0900

    Add files via upload

--- level_editor.py
+++ level_editor.py
@@ -49,7 +49,7 @@ class MYADDON_OT_stretch_vertex(bpy.types.Operator):
 
 # --- オペレータ：ICO球生成 ---
 class MYADDON_OT_create_ico_sphere(bpy.types.Operator):
-    bl_idname = "myaddon.myaddon_ot_create_object"
+    bl_idname = "myaddon.myaddon_ot_create_object" # 元のIDを維持
     bl_label = "ICO球生成"
     bl_description = "ICO球を生成します"
     bl_options = {'REGISTER', 'UNDO'}
@@ -64,64 +64,93 @@ class MYADDON_OT_export_scene(bpy.types.Operator, bpy_extras.io_utils.ExportHelp
     bl_label = "シーン出力"
     bl_description = "シーン情報をExportします"
 
-    # 資料より：出力するファイルの拡張子を指定
     filename_ext = ".scene"
 
     def write_and_print(self, file, str):
-        """資料より：コンソールとファイルに同時に書き出す関数"""
         print(str)
         file.write(str + '\n')
 
     def parse_scene_recursive(self, file, object, level):
-        """資料より：親子関係を辿る再帰関数（深さ優先探索）"""
-        # 深さに応じてインデント（タブ）を作成
         indent = ''
         for i in range(level):
             indent += "\t"
 
-        # オブジェクト名を出力
         self.write_and_print(file, indent + object.type + " - " + object.name)
         
-        # トランスフォーム情報の抽出
         trans, rot, scale = object.matrix_local.decompose()
         rot = rot.to_euler()
         rot.x, rot.y, rot.z = [math.degrees(v) for v in rot]
 
-        # 情報を書き込み
         self.write_and_print(file, indent + "Trans(%f,%f,%f)" % (trans.x, trans.y, trans.z))
         self.write_and_print(file, indent + "Rot(%f,%f,%f)" % (rot.x, rot.y, rot.z))
         self.write_and_print(file, indent + "Scale(%f,%f,%f)" % (scale.x, scale.y, scale.z))
-        self.write_and_print(file, '') # 空行
+        self.write_and_print(file, '')
 
-        # 子オブジェクトに対して再帰呼び出し
         for child in object.children:
             self.parse_scene_recursive(file, child, level + 1)
 
     def export(self):
-        """資料より：ファイル書き出しのメイン処理"""
         print("シーン情報出力開始... %r" % self.filepath)
-        
         with open(self.filepath, "wt") as file:
             self.write_and_print(file, "SCENE")
-            # ルートオブジェクト（親がいないもの）から探索開始
             for object in bpy.context.scene.objects:
                 if object.parent is None:
                     self.parse_scene_recursive(file, object, 0)
 
     def execute(self, context):
         print("シーン情報をExportします")
-        self.export() # ファイル書き出し実行
-        
+        self.export()
         self.report({'INFO'}, "シーン情報をExportしました")
-        print("シーン情報をExportしました")
         return {'FINISHED'}
+
+# --- ★追加：オペレータ：カスタムプロパティ['file_name']追加 (スライド10) ---
+class MYADDON_OT_add_filename(bpy.types.Operator):
+    bl_idname = "myaddon.myaddon_ot_add_filename"
+    bl_label = "FileName 追加"
+    bl_description = "['file_name']カスタムプロパティを追加します"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    def execute(self, context):
+        # 選択オブジェクトにプロパティを追加
+        context.object["file_name"] = ""
+        return {'FINISHED'}
+
+# --- ★追加：パネルクラス (スライド9, 11-14) ---
+class OBJECT_PT_file_name(bpy.types.Panel):
+    """オブジェクトのファイルネームパネル"""
+    bl_idname = "OBJECT_PT_file_name"
+    bl_label = "FileName"
+    bl_space_type = "PROPERTIES"
+    bl_region_type = "WINDOW"
+    bl_context = "object"
+
+    def draw(self, context):
+        layout = self.layout
+        obj = context.object
+
+        # スライド9：既存のオペレータを表示
+        layout.operator(MYADDON_OT_stretch_vertex.bl_idname, text=MYADDON_OT_stretch_vertex.bl_label)
+        layout.operator(MYADDON_OT_create_ico_sphere.bl_idname, text=MYADDON_OT_create_ico_sphere.bl_label)
+        layout.operator(MYADDON_OT_export_scene.bl_idname, text=MYADDON_OT_export_scene.bl_label)
+
+        layout.separator()
+
+        # スライド12-14：カスタムプロパティの有無による表示切り替え
+        if "file_name" in obj:
+            # プロパティがあれば入力欄を表示
+            layout.prop(obj, '["file_name"]', text="FileName:")
+        else:
+            # プロパティがなければ追加ボタンを表示
+            layout.operator(MYADDON_OT_add_filename.bl_idname)
 
 # --- 登録処理 ---
 classes = (
     MYADDON_OT_stretch_vertex,
     MYADDON_OT_create_ico_sphere,
     MYADDON_OT_export_scene,
+    MYADDON_OT_add_filename,
     TOPBAR_MT_my_menu,
+    OBJECT_PT_file_name,
 )
 
 def register():
@@ -132,7 +161,7 @@ def register():
 
 def unregister():
     bpy.types.TOPBAR_MT_editor_menus.remove(TOPBAR_MT_my_menu.submenu)
-    for cls in classes:
+    for cls in reversed(classes): # 登録解除は逆順が安全
         bpy.utils.unregister_class(cls)
     print("レベルエディタが無効化されました。")
 
